2adefeeba5ebdb4874ed1fc5ae4ca72c79ff6ae7
Nikikosa <nikitakosyuk1@gmail.com>
2025-09-16 23:12:54 -0700

    introduce cube manager.

---
+++ src/MinesweeperEngine/CubeManager.java
@@ -0,0 +1,13 @@
+package MinesweeperEngine;
+
+public class CubeManager {
+    private int numCubes;
+    private int width;
+    private int height;
+
+    public CubeManager(int numCubes, int width, int height) {
+        this.numCubes = numCubes;
+        this.width = width;
+        this.height = height;
+    }
+}
